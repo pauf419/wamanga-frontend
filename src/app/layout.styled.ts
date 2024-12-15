980cd3eb5abf9097e2fe6019e2e64c8b6a1d2330
xndrpr <velaton618@gmail.com>
2024-12-15 16:31:27 +0800

    fix stylelint

--- src/app/layout.styled.ts
+++ src/app/layout.styled.ts
@@ -4,8 +4,8 @@ import { colors } from "@/const";
 import styled from "@emotion/styled";
 
 export const LayoutSC = styled.div`
-  background: ${colors.background};
   display: grid;
   grid-template-columns: auto 1fr;
   height: 100vh;
+  background: ${colors.background};
 `;
