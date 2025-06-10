bebe743bea0d2364326e7889b3884db51021b37f
pauf419 <lebedevpavel0511@gmail.com>
2025-06-10 17:57:37 +0300

    Test metrica

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,5 @@
+"use server";
+
 import React from "react";
 import {
   Content,
@@ -14,8 +16,11 @@ import Logo from "@icons/svg/logo.svg";
 import Telegram from "@icons/svg/telegram.svg";
 import Instagram from "@icons/svg/instagram.svg";
 import Tiktok from "@icons/svg/tiktok.svg";
+import { getSettings } from "@/api/settings";
 
-const Footer = () => {
+const Footer = async () => {
+  const settings = await getSettings();
+
   return (
     <FooterSC>
       <Content>
@@ -46,6 +51,32 @@ const Footer = () => {
       <Copyright>
         © 2024 WaManga - WaManga@bk.ru - почта для вопросов
       </Copyright>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(102485920, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+              });
+            `,
+        }}
+      />
+      <noscript>
+        <div>
+          <img
+            src="https://mc.yandex.ru/watch/102485920"
+            style={{ position: "absolute", left: "-9999px" }}
+            alt=""
+          />
+        </div>
+      </noscript>
     </FooterSC>
   );
 };
