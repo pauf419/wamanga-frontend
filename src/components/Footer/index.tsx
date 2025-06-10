feb89be0e5875c640b54f7a1ce2b75112a9da73a
pauf419 <lebedevpavel0511@gmail.com>
2025-06-10 21:56:14 +0300

    Google analytics

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -77,6 +77,16 @@ const Footer = async () => {
           />
         </div>
       </noscript>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GCBZHB4YVB');
+          `,
+        }}
+      />
     </FooterSC>
   );
 };
