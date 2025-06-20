2d408b7cdd93cf99144116e7c5bb1fd06a0b62e9
pauf419 <lebedevpavel0511@gmail.com>
2025-06-20 21:23:59 +0300

    Fix yandex ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,32 @@ export default function RootLayout({
             {children}
           </LayoutSC>
         </Providers>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(102485920, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+              });
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/102485920"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
